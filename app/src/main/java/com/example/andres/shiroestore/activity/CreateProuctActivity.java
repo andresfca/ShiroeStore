a2e5b4ee26f4e074fbe5bcc81ab6069d3b3649d2
Andres Cantillo <acantill26@gmail.com>
2017-11-18 18:38:25 -0500

    create item interface done

--- app/src/main/java/com/example/andres/shiroestore/activity/CreateProuctActivity.java
+++ app/src/main/java/com/example/andres/shiroestore/activity/CreateProuctActivity.java
@@ -3,10 +3,19 @@ package com.example.andres.shiroestore.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.andres.shiroestore.R;
 
-public class CreateProuct extends AppCompatActivity {
+public class CreateProuctActivity extends AppCompatActivity {
+
+    private EditText name;
+    private Spinner category;
+    private EditText price;
+    private EditText cant;
+    private String photo;
+    private String detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

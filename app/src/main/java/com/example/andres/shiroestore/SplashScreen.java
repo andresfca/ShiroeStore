ced0df439f575b2e45ad3279748560b20820bca3
Andres Cantillo <acantill26@gmail.com>
2017-11-21 03:30:40 -0500

    splash terminado

--- app/src/main/java/com/example/andres/shiroestore/SplashScreen.java
+++ app/src/main/java/com/example/andres/shiroestore/SplashScreen.java
@@ -1,7 +1,12 @@
 package com.example.andres.shiroestore;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.andres.shiroestore.activity.login.MainActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        MediaPlayer mp = MediaPlayer.create(SplashScreen.this, R.raw.coc);
+        mp.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
     }
 }

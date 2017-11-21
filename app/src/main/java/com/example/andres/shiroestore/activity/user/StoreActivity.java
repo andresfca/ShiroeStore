619a997ff11d770cb0cd47a26bbdcdd1ff3e99be
Andres Cantillo <acantill26@gmail.com>
2017-11-21 00:23:12 -0500

    actualizado

--- app/src/main/java/com/example/andres/shiroestore/activity/user/StoreActivity.java
+++ app/src/main/java/com/example/andres/shiroestore/activity/user/StoreActivity.java
@@ -1,7 +1,11 @@
 package com.example.andres.shiroestore.activity.user;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.example.andres.shiroestore.R;
 
@@ -11,5 +15,10 @@ public class StoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
+
